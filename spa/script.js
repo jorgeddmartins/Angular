66cdad82d1f91113abfdef9758e81b27c4ccb0d6
Jorge DD Martins <jorgedd.martins@hotmail.com>
2014-04-28 14:06:31 +0100

    SPA with Angular
    Single Page Aplication System

---
+++ spa/script.js
@@ -0,0 +1,57 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', []);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			})
+			
+			// route for the speak page
+			.when('/speak', {
+				templateUrl : 'pages/speak.html',
+				controller  : 'speakController'
+			});
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
+	
+	scotchApp.controller('speakController', function($scope) {
+		$scope.speak = 'Speak with us, We are the best.';
+		$scope.down = 'Yes we are, for sure!!!! :)';
+	});
+	
+	
+	
+	
+	
+	
+	
